f0e0a1d5890690e1303b3fa6d67d5cc9c29c51df
Koki Nakamura <nk.levia22@gmail.com>
2021-11-23 22:44:27 +0900

    managing multiple functions

---
+++ test-func/functions/get_today/handler.py
@@ -0,0 +1,23 @@
+import json
+from common.date import get_today_date
+
+def hello(event, context):
+    body = {
+        "today": str(get_today_date())
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
